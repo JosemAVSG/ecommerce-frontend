73da02d58e0c1d2cd99efcdd6fcca89c263d0cd7
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-07-14 13:54:44 -0500

    config: 'adding configuration to middleware'

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -61,7 +61,7 @@ const LoginForm =  () => {
                     type="email"
                     name="email"
                     placeholder="Email"
-                    className={`peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-white data-[status=true]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-white dark:autofill:shadow-autofill dark:peer-focus:text-white [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0`}
+                    className={`peer block min-h-[auto]  w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-white data-[status=true]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-white dark:autofill:shadow-autofill dark:peer-focus:text-white [&:not([data-status=true])]:placeholder:opacity-0`}
                   /> 
                   <label data-status = {valid ? "true" : "false"} className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-white transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-white peer-data-[status=true]:-translate-y-[1.15rem] peer-data-[status=true]:scale-[0.8] motion-reduce:transition-none dark:text-white dark:peer-focus:text-white">
                     Email
